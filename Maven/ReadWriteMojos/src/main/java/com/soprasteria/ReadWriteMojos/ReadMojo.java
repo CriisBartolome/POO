7b4424f5cc5b0c7c17fc2f48d62da092b73ec4c1
cbartolome <cristina.bartolome@soprasteria.com>
2021-03-31 09:46:27 +0200

    Ejemplo de profiles en Maven

---
+++ Maven/ReadWriteMojos/src/main/java/com/soprasteria/ReadWriteMojos/ReadMojo.java
@@ -0,0 +1,52 @@
+package com.soprasteria.ReadWriteMojos;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.File;
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal readFiles
+ * 
+ * @phase package
+ */
+public class ReadMojo extends AbstractMojo{
+    /**
+     * Location of the file.
+     * @parameter default-value="${project.build.directory}"
+     */
+	private String ruta;
+
+    public void execute() throws MojoExecutionException{
+    	File archivo = new File(ruta);
+    	String[] listado = archivo.list();
+    	if (listado == null || listado.length == 0) {
+    	    System.out.println("No hay elementos dentro de la carpeta actual");
+    	    return;
+    	}
+    	else {
+    	    for (int i=0; i< listado.length; i++) {
+    	        System.out.println(listado[i]);
+    	    }
+    	}
+    }
+    
+}
