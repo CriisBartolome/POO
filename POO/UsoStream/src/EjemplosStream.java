8cc178d1475cc7dd1ab4c6378c6acd3b251d1e4e
Cristina <criisandfriends@gmail.com>
2021-05-06 16:08:44 +0200

    Arreglos Stream

--- POO/UsoStream/src/EjemplosStream.java
+++ POO/UsoStream/src/EjemplosStream.java
@@ -14,27 +14,27 @@ public class EjemplosStream {
                     .forEach(System.out::println);
         
         //Ejemplo2 -> filtrar los elementos mayores que un valor
-        System.out.println("Ejemplo2: mostrar los n˙meros de la lista mayores que 4");
+        System.out.println("Ejemplo2: mostrar los n√∫meros de la lista mayores que 4");
         listaEnteros.stream()
                     .filter(valor -> valor > 4)
                     .forEach(valor -> System.out.println(valor));
         
         //Ejemplo3 -> mostrar el doble de los elementos
-        System.out.println("Ejemplo3: mostrar los n˙meros de la lista multiplicados por 2");
+        System.out.println("Ejemplo3: mostrar los n√∫meros de la lista multiplicados por 2");
         listaEnteros.stream()
                     .map(valor -> valor*2)
                     .forEach(valor -> System.out.println(valor));
         
         //Ejemplo4 -> sumar todos los numeros
-        System.out.println("Ejemplo4: sumar los n˙meros de la lista");
+        System.out.println("Ejemplo4: sumar los n√∫meros de la lista");
         int suma = listaEnteros.stream()
-                                .reduce(0, Integer::sum);
+                               .reduce(0, Integer::sum);
         System.out.println(suma);
         
-        //Ejemplo4 -> contar cu·ntos numeros hay en la lista
+        //Ejemplo5 -> contar cu√°ntos numeros hay en la lista
         System.out.println("Ejemplo5: elementos contenidos en la lista");
         System.out.println(listaEnteros.stream()
-                                .count());
+                                       .count());
 
     }
 
