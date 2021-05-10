889851d3872b62dbbdc7284e8e03ed55d557de4f
Cristina <criisandfriends@gmail.com>
2021-05-10 09:21:43 +0200

    Mejora ejemplo stub

--- POO/TestDobles/src/StubTest.java
+++ POO/TestDobles/src/StubTest.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -6,7 +8,25 @@ public class StubTest {
     //Objetos parecidos a los dummy pero que proporcionan respuestas predefinidas 
     //a llamadas realizadas durante los test.
     
-    private class Temperatura {
+    public interface Temperatura{
+        int leerTemperatura();
+        boolean haceCalor();
+    }
+    
+    private class TemperaturaReal implements Temperatura{
+
+        //He definido un valor fijo de temperatura porque quiero probar el método haceCalor()
+        //No tengo un método que obtenga la temperatura real por lo que le doy un valor por defecto
+        public int leerTemperatura() {
+            return new Random().nextInt();
+        }
+        
+        public boolean haceCalor() {
+            return leerTemperatura() > 25;            
+        }
+    }
+    
+    private class TemperaturaStub implements Temperatura{
 
         //He definido un valor fijo de temperatura porque quiero probar el método haceCalor()
         //No tengo un método que obtenga la temperatura real por lo que le doy un valor por defecto
@@ -19,9 +39,24 @@ public class StubTest {
         }
     }
     
+    public class Meteorologia{
+        
+        Temperatura temperatura;
+        
+        public Meteorologia(Temperatura temperatura) {
+            this.temperatura = temperatura;
+        }
+        
+        public void mostrarTemperatura() {
+            System.out.println(temperatura.leerTemperatura());
+        }
+    }
+    
     @Test
     public void haceCalor() {
-        Temperatura temperatura = new Temperatura();
+        Temperatura temperatura = new TemperaturaStub();
+        Meteorologia meteo = new Meteorologia(temperatura);
+        meteo.mostrarTemperatura();
         Assert.assertEquals(false, temperatura.haceCalor());
     }
 
