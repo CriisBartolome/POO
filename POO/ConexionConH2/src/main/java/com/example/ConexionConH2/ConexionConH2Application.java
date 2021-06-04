5ccbad5846b3f2366c4c4270c306d1cf0626e8b2
Cristina <criisandfriends@gmail.com>
2021-06-04 11:18:12 +0200

    Ejemplo  Spring conexión con H2

---
+++ POO/ConexionConH2/src/main/java/com/example/ConexionConH2/ConexionConH2Application.java
@@ -0,0 +1,13 @@
+package com.example.ConexionConH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionConH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionConH2Application.class, args);
+	}
+
+}
