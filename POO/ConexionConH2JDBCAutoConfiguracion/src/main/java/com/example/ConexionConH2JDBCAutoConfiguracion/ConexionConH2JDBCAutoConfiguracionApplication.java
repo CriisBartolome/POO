b83f033de651d4d5101440ad7597e35ee81afab0
Cristina <criisandfriends@gmail.com>
2021-06-09 16:59:44 +0200

    Ejemplo Spring conexion con H2 usando JDBC
     Auto configuración

---
+++ POO/ConexionConH2JDBCAutoConfiguracion/src/main/java/com/example/ConexionConH2JDBCAutoConfiguracion/ConexionConH2JDBCAutoConfiguracionApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConexionConH2JDBCAutoConfiguracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionConH2JDBCAutoConfiguracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionConH2JDBCAutoConfiguracionApplication.class, args);
+	}
+
+}
---
+++ POO/ConexionConH2JDBCAutoConfiguracion/src/test/java/com/example/ConexionH2JDBC/ConexionH2JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConexionH2JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexionH2JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
