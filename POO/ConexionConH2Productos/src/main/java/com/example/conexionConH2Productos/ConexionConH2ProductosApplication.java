cdb533801a41bdaf14944aa0f8902e700f10371d
Cristina <criisandfriends@gmail.com>
2021-06-09 12:23:23 +0200

    Ejemplo Spring conexion H2 persistencia JPA
    Ejemplo paginación para mostrar los resultados de las consultas

--- POO/ConexionConH2Productos/src/main/java/com/example/conexionConH2Productos/ConexionConH2ProductosApplication.java
+++ POO/ConexionConH2Productos/src/main/java/com/example/conexionConH2Productos/ConexionConH2ProductosApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
