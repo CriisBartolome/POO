7f9c25190d2854ec93e05e5b2a7653465cab27c3
Cristina <criisandfriends@gmail.com>
2021-06-14 16:30:06 +0200

    Ejemplo Servicio REST con put y post

---
+++ POO/ServicioREST/src/main/java/com/example/ServicioREST/Producto.java
@@ -0,0 +1,23 @@
+package com.example.ServicioREST;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Productos")
+public class Producto {
+
+    private String nombre;
+    @Id
+    private Integer id;
+    private Double precio;
+    private String categoria;
+}
