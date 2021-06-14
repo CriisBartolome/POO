fff4c2258e5845e485b188de98e799696e28f2a2
Cristina <criisandfriends@gmail.com>
2021-06-14 14:39:37 +0200

    Mejora ejemplo Spring AOP

--- POO/SpringAOP/src/main/java/com/example/SpringAOP/Mensaje.java
+++ POO/SpringAOP/src/main/java/com/example/SpringAOP/Mensaje.java
@@ -7,9 +7,13 @@ public class Mensaje {
     
     public void escribirMensajeHola() {
         System.out.println("Hola");
+        metodo();
     }
     public void escribirMensajeMundo() {
         System.out.println("Mundo");
+    }
+    protected void metodo() {
+        System.out.println("Metodo protected");
     }
 
 }
--- POO/SpringAOP/src/main/java/com/example/SpringAOP/SpringAopApplication.java
+++ POO/SpringAOP/src/main/java/com/example/SpringAOP/SpringAopApplication.java
@@ -6,14 +6,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass=true)
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SpringAopApplication {
 
-	public static void main(String[] args) {
-	    ApplicationContext context = SpringApplication.run(SpringAopApplication.class, args);
-        Mensaje empleado = context.getBean(Mensaje.class); 
-		empleado.escribirMensajeHola();
-		empleado.escribirMensajeMundo();
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringAopApplication.class, args);
+        Mensaje mensaje = context.getBean(Mensaje.class);
+        // Los aspectos solo se aplican si desde otra clase se llama a los
+        // métodos a los que se quiere hacer referencia
+        // Estos métodos pueden ser public o protected
+        // Pero si nosotros llamamos a un método dentro de la misma clase, me da
+        // igual si es public, protected o private, que el aspecto no se va a
+        // aplicar a ese método
+        mensaje.metodo();
+        mensaje.escribirMensajeHola();
+        mensaje.escribirMensajeMundo();
+    }
 
 }
