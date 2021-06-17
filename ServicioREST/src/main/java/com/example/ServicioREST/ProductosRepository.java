3c6bb142bec6220a8a627857daca9f378cdc6214
Cristina <criisandfriends@gmail.com>
2021-06-17 11:36:23 +0200

    Delete ServicioREST directory

--- ServicioREST/src/main/java/com/example/ServicioREST/ProductosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ServicioREST;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductosRepository extends JpaRepository<Producto, Integer> {
-}
