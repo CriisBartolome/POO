bfae05f3928adbb77b78a54d3c1a86878f67648d
Cristina <criisandfriends@gmail.com>
2021-06-17 11:37:09 +0200

    Mejora ejemplo servicio REST. Validación de campos

--- POO/ServicioREST/src/main/java/com/example/ServicioREST/Producto.java
+++ POO/ServicioREST/src/main/java/com/example/ServicioREST/Producto.java
@@ -3,6 +3,7 @@ package com.example.ServicioREST;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,9 +16,13 @@ import lombok.NoArgsConstructor;
 @Table(name = "Productos")
 public class Producto {
 
+    // Los nombres de los productos tienen que comenzar por la letra C mayúscula
+    @Pattern(regexp = "^C.*", message = "El nombre tiene que empezar por C mayúscula")
     private String nombre;
     @Id
-    private Integer id;
+    // Solo puede recibir enteros, tanto positivos como negativos
+    @Pattern(regexp = "-?[0-9]*", message = "El id solo puede ser un número entero")
+    private String id;
     private Double precio;
     private String categoria;
 }
