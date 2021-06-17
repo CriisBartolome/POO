af146dd581a162204c4734b45cb7e867d7718dc2
Cristina <criisandfriends@gmail.com>
2021-06-17 11:35:13 +0200

    Mejora ejemplo servicio REST. Comprobación campos

---
+++ ServicioREST/src/main/java/com/example/ServicioREST/Producto.java
@@ -0,0 +1,28 @@
+package com.example.ServicioREST;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Productos")
+public class Producto {
+
+    // Los nombres de los productos tienen que comenzar por la letra C mayúscula
+    @Pattern(regexp = "^C.*", message = "El nombre tiene que empezar por C mayúscula")
+    private String nombre;
+    @Id
+    // Solo puede recibir enteros, tanto positivos como negativos
+    @Pattern(regexp = "-?[0-9]*", message = "El id solo puede ser un número entero")
+    private String id;
+    private Double precio;
+    private String categoria;
+}
