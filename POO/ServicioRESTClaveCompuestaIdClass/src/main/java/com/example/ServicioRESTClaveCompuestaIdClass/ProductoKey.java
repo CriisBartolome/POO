5672881bc5ae4c8873d6bd0038afb5de70e3be2d
Cristina <criisandfriends@gmail.com>
2021-06-25 14:33:20 +0200

    Mejora ejemplo servicio REST key compuesta @IdClass

--- POO/ServicioRESTClaveCompuestaIdClass/src/main/java/com/example/ServicioRESTClaveCompuestaIdClass/ProductoKey.java
+++ POO/ServicioRESTClaveCompuestaIdClass/src/main/java/com/example/ServicioRESTClaveCompuestaIdClass/ProductoKey.java
@@ -2,8 +2,6 @@ package com.example.ServicioRESTClaveCompuestaIdClass;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Pattern;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductoKey implements Serializable {
 
-    @Pattern(regexp = "^C.*", message = "El nombre tiene que empezar por C mayúscula")
     private String nombre;
     private Integer id;
 }
