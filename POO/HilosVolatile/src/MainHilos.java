f9adcb284a917fce0b969d6fe5bdd2c71534d289
Cristina <criisandfriends@gmail.com>
2021-07-01 08:45:02 +0200

    Mejora ejemplo Volatile Hilos

--- POO/HilosVolatile/src/MainHilos.java
+++ POO/HilosVolatile/src/MainHilos.java
@@ -1,19 +1,26 @@
 
 public class MainHilos {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+
+        // En este código se puede ver que el valor actual siempre se actualiza
+        // bien, no hay números repetidos
+
         VolatileContador vContador = new VolatileContador();
         Thread hilo1 = new VolatileThread(vContador);
         Thread hilo2 = new VolatileThread(vContador);
+        Thread hilo3 = new VolatileThread(vContador);
+        Thread hilo4 = new VolatileThread(vContador);
 
         hilo1.start();
         hilo2.start();
-        
-        while(hilo1.isAlive() && hilo2.isAlive()) {
-            
+        hilo3.start();
+        hilo4.start();
+
+        while (hilo1.isAlive() && hilo2.isAlive() && hilo3.isAlive() && hilo4.isAlive()) {
+
         }
-        
+
     }
 
 }
