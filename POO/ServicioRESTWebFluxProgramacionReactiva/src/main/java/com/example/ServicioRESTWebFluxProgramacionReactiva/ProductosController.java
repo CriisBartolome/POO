ad7464e01a27892a90844eedb304d10dc6a4dd26
Cristina <criisandfriends@gmail.com>
2021-07-07 13:10:41 +0200

    Ejemplo servicio REST usando p. reactiva Webflux

---
+++ POO/ServicioRESTWebFluxProgramacionReactiva/src/main/java/com/example/ServicioRESTWebFluxProgramacionReactiva/ProductosController.java
@@ -0,0 +1,30 @@
+package com.example.ServicioRESTWebFluxProgramacionReactiva;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ProductosController {
+
+    @Autowired
+    ProductosRepository productosRepository;
+
+    @GetMapping
+    public Flux<Producto> getProductos() {
+        return Flux.fromIterable(productosRepository.findAll());
+    }
+
+    @PostMapping
+    public Mono<Producto> postProductos(@RequestBody @Valid Producto producto) {
+        return Mono.just(productosRepository.save(producto));
+    }
+
+}
