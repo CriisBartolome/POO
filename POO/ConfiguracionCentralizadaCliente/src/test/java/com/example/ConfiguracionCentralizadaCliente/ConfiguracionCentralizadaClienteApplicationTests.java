ca7f23e22c3d4c20d4e29d04a98fe1d0f3f1a8e4
Cristina <criisandfriends@gmail.com>
2021-07-26 13:19:15 +0200

    Ejemplo Configuración Centralizada Cliente

---
+++ POO/ConfiguracionCentralizadaCliente/src/test/java/com/example/ConfiguracionCentralizadaCliente/ConfiguracionCentralizadaClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConfiguracionCentralizadaCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracionCentralizadaClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
